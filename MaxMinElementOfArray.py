9c935672bfad885c7d9c6748f799efd382981878
Loulou-lepou <59030304+Loulou-lepou@users.noreply.github.com>
2020-05-31 19:11:28 +0700

    Find max and min element of a given 1-D array
    Method 1 : Simple Linear Search
    1) Initialize e_min, e_max as the first 2 elements
    2) Starting from 3rd element, compare each element with e_min, e_max 
    & change e_max, e_min accordingly

---
+++ MaxMinElementOfArray.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Find maximum and minimum of an array - Simple Linear Search
+# Time complexity : O(n)
+
+
+def get_min_max(arr: list):
+
+    if len(arr) == 1:
+        return arr[0], arr[0]
+
+    if arr[0] > arr[1]:
+        e_min, e_max = arr[0], arr[1]
+    else:
+        e_min, e_max = arr[1], arr[0]
+
+    for i in range(2, len(arr)):
+        if arr[i] > e_max:
+            e_max = arr[i]
+        elif arr[i] < e_min:
+            e_min = arr[i]
+
+    return e_max, e_min
+
+
+if __name__ == '__main__':
+    L = [1000, 11, 445, 1, 330, 3000]
+    (max_element, min_element) = get_min_max(L)
+    print("Minimum element is ", min_element)
+    print("Maximum element is ", max_element)
