1698c06c8ccf0cf91ce508dede75837fda4d3c3b
Loulou-lepou <59030304+Loulou-lepou@users.noreply.github.com>
2020-06-01 09:04:30 +0700

    update MaxMinElementOfArray.py

--- MaxMinElementOfArray.py
+++ MaxMinElementOfArray.py
@@ -9,9 +9,9 @@ def get_min_max(arr: list):
         return arr[0], arr[0]
 
     if arr[0] > arr[1]:
-        e_min, e_max = arr[0], arr[1]
-    else:
         e_min, e_max = arr[1], arr[0]
+    else:
+        e_min, e_max = arr[0], arr[1]
 
     for i in range(2, len(arr)):
         if arr[i] > e_max:
@@ -19,11 +19,11 @@ def get_min_max(arr: list):
         elif arr[i] < e_min:
             e_min = arr[i]
 
-    return e_max, e_min
+    return e_min, e_max
 
 
 if __name__ == '__main__':
     L = [1000, 11, 445, 1, 330, 3000]
-    (max_element, min_element) = get_min_max(L)
+    (min_element, max_element) = get_min_max(L)
     print("Minimum element is ", min_element)
     print("Maximum element is ", max_element)
