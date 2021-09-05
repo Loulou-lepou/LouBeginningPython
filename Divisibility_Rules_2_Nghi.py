0e8ef95f56aecbecbf8b78c14379b023f60fc1de
Nguyen Thi Thu Hoa <59030304+Loulou-lepou@users.noreply.github.com>
2021-09-05 12:09:35 +0700

    Update Divisibility_Rules_2_Nghi.py

--- Divisibility_Rules_2_Nghi.py
+++ Divisibility_Rules_2_Nghi.py
@@ -7,12 +7,11 @@ def divisible_by_seven(n: str) -> bool:
     # = [10^0, 10^1, 10^2, 10^3, 10^4, 10^5] mod 7
     pow10s = [1, 3, 2, -1, -3, -2]
     total = 0
-    k = len(n)
-    pos = k - 1
+    pos = len(n) - 1
     i_10 = 0
     for i in range(pos, -1, - 1):
         total = (total + (int(n[i]) * pow10s[i_10]) % 7) % 7
-        # print('{0} {1} {2} {3}'.format(i, n[i], total, i_10))
+        # print(f'{i} {n[i]} {total} {i_10}')
         i_10 = (i_10 + 1) % 6
     print(f'Remainder of {int(n)} when dividing by 7 = {total}')
     return total == 0
@@ -23,7 +22,7 @@ if __name__ == '__main__':
     user_list = input('Enter elements of a list separated by space').split(' ')
     for num in user_list:
         if divisible_by_seven(num):
-            print('{0} is divisible by 7'.format(num))
+            print(f'{num} is divisible by 7')
         else:
-            print('{0} is NOT divisible by 7'.format(num))
+            print(f'{num} is NOT divisible by 7')
         print('=============================')
